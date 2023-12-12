1bbeaa58c85b0556afba768ac026b1fd2a1f812f
mrozek <01178582@pw.edu.pl>
2023-12-12 18:35:04 +0100

    conflict commit

--- db.js
+++ db.js
@@ -3,8 +3,8 @@ const {Pool} = require('pg');
 const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
-  database: 'pap',
-  password: 'Praca12345.',
+  database: 'pap-project',
+  password: 'postgres',
   port: 5432,
 });
 
