61d004675342119de201e23767f2c22ab5df654e
Marcin Lisowski <mnlisows@gmail.com>
2024-01-05 20:10:31 +0100

    post_like, get_like endpoints added

--- controllers/exercisePageController.js
+++ controllers/exercisePageController.js
@@ -2,6 +2,7 @@
 const pool = require('../db')
 
 const get_exercise_page = async (req, res) => {
+	console.log(req.session.userid)
 	try {
 		const exerciseId = req.params.exercise_id;
 		const result = await pool`
@@ -11,6 +12,57 @@ const get_exercise_page = async (req, res) => {
 		WHERE e.exercise_id = ${exerciseId}`;
 		res.json(result[0]);
 	} catch (error) {
+		console.error(error);	
+		res.status(500).json({ error: 'Internal server error'});
+	}
+};
+
+const post_like = async(req, res) => {
+
+	console.log('dsad');
+	try {
+		const userId = req.session.userId;
+		const exerciseId = req.params.exercise_id;
+		const vote = req.body.vote;
+		console.log(userId);
+		console.log(exerciseId);
+		console.log(vote);
+		const insert = await pool`INSERT INTO ex_users (user_id, exercise_id, vote)
+		VALUES (${userId}, ${exerciseId}, ${vote})
+		ON CONFLICT (user_id, exercise_id) DO UPDATE
+		SET vote = ${vote};`;
+
+		const result = await pool`
+		SELECT 
+			SUM(CASE WHEN vote = 1 THEN 1 ELSE 0 END) AS likes,
+    		SUM(CASE WHEN vote = -1 THEN 1 ELSE 0 END) AS dislikes
+		FROM ex_users
+		GROUP BY EXERCISE_ID
+		HAVING EXERCISE_ID = ${exerciseId};`;
+		console.log(result[0]);
+		res.json(result[0]);
+	}
+	catch (error) {
+		console.error(error);
+		res.status(500).json({ error: 'Internal server error'});
+	}
+};
+
+const get_like = async (req, res) => {
+	
+	console.log('sadsadasdsc x');
+	try {
+		const exerciseId = req.params.exercise_id;
+		const result = await pool`
+		SELECT 
+			SUM(CASE WHEN vote = 1 THEN 1 ELSE 0 END) AS likes,
+    		SUM(CASE WHEN vote = -1 THEN 1 ELSE 0 END) AS dislikes
+		FROM ex_users
+		GROUP BY EXERCISE_ID
+		HAVING EXERCISE_ID = ${exerciseId};`;
+		console.log(result[0]);
+		res.json(result[0]);
+	} catch (error) {
 		console.error(error);
 		res.status(500).json({ error: 'Internal server error'});
 	}
@@ -18,4 +70,6 @@ const get_exercise_page = async (req, res) => {
 
 module.exports = {
 	get_exercise_page,
+	post_like,
+	get_like,
 }
