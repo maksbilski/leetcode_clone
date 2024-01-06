ff7f3afa9a230d2a93f03a0cd05cffcacff99f11
Marcin Lisowski <mnlisows@gmail.com>
2024-01-06 23:01:22 +0100

    routes setup for calendar and history

--- routers/profile.js
+++ routers/profile.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const {get_aggregate_stats} = require('../controllers/profileController')
+const {get_aggregate_stats, get_calendar, get_history} = require('../controllers/profileController')
 
 router.route('/aggregate').get(get_aggregate_stats);
+router.route('/calendar').get(get_calendar);
+router.route('/history').get(get_history);
 
 
 module.exports = router
