75210de3a75116d009d770f123e9d0cc2452f2e2
Marcin Lisowski <mnlisows@gmail.com>
2024-01-07 18:28:15 +0100

    checking new exercises for each user

--- services/generateUserTasks.js
+++ services/generateUserTasks.js
@@ -1,15 +1,17 @@
-const { pool } = require('./db');
+const pool  = require('../db');
 
 async function generateUserTasks() {
   try {
-    const users = await pool.query('SELECT id, last_login FROM users');
+    console.log('intervalll')
+    const users = await pool`SELECT * FROM users;`;
+    //console.log(users);
     let tasksForUsers = {};
 
-    for (const user of users.rows) {
-      const tasks = await pool.query('SELECT * FROM exercises WHERE date_added > $1', [user.last_login]);
-      tasksForUsers[user.id] = tasks.rows;
+    for (const user of users) {
+      const tasks = await pool`SELECT * FROM exercises WHERE date_added > ${user.last_login}`;
+      tasksForUsers[user.email] = tasks;
     }
-
+    //console.log(tasksForUsers);
     return tasksForUsers;
   } catch (err) {
     console.error(err);
