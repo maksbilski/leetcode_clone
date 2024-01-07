bac8570675b978440817c48bd3fd11f102189345
mrozek <01178582@pw.edu.pl>
2024-01-07 01:04:26 +0100

    exercisePage Submit button update

--- routers/exercisePage.js
+++ routers/exercisePage.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
 
-const {get_exercise_page} = require('../controllers/exercisePageController')
+const {get_exercise_page, submit_code} = require('../controllers/exercisePageController')
 
 router.route('/:exercise_id').get(get_exercise_page);
+router.route('/:exercise_id/submit').post(submit_code);
 
 module.exports = router
