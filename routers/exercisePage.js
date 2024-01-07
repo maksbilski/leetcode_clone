3b0997e0fdc97bc2abc9532225c9e89b1a5c74a1
mrozek <01178582@pw.edu.pl>
2024-01-07 15:28:42 +0100

    camelCase correction

--- routers/exercisePage.js
+++ routers/exercisePage.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const router = express.Router()
 
-const {get_exercise_page, run_code} = require('../controllers/exercisePageController')
+const {getExercisePage, runCode} = require('../controllers/exercisePageController')
 
-router.route('/:exercise_id').get(get_exercise_page);
-router.route('/:exercise_id/run_code').post(run_code);
+router.route('/:exercise_id').get(getExercisePage);
+router.route('/:exercise_id/run_code').post(runCode);
 
 module.exports = router
