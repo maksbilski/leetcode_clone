65326445e224b0fad2decb9731f71f445ee970e8
nasekajlo <01184761@pw.edu.pl>
2024-01-07 20:32:15 +0100

    register+login

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const session = require('express-session'); // Добавим импорт express-session
-// Импорт функции checkAuthentication
+const session = require('express-session');
 const { checkAuthentication } = require('./controllers/loginController');
-
-//db connection
-const pool = require('./db');
-module.exports = { pool };
+const { pool } = require('./db');
 
 const app = express();
 const port = 5000;
@@ -15,37 +11,33 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 
 const secret_key = 'PaP2023z';
+
 app.use(session({
   secret: secret_key,
   resave: false,
   saveUninitialized: true,
+  cookie: {
+    maxAge: 30000, // 30 sec in milliseconds
+  },
 }));
 
 app.use('/api/exercises', checkAuthentication);
 app.use('/api/statistics', checkAuthentication);
 
-//setup routers
+// Setup routers
 const exercisesRouter = require('./routers/exercises');
-const statisticsRouter = require('./routers/statistics')
-const pagesRouter = require('./routers/SSR_pages')
-const registerRouter = require('./routers/register')
-const exercisePageRouter = require('./routers/exercisePage')
-const login = require('./routers/login')
+const statisticsRouter = require('./routers/statistics');
+const pagesRouter = require('./routers/SSR_pages');
+const registerRouter = require('./routers/register');
+const exercisePageRouter = require('./routers/exercisePage');
+const loginRouter = require('./routers/login');
 
-app.use('/api/exercises', exercisesRouter)
-app.use('/api/statistics', statisticsRouter)
-app.use('/api/login', login)
-app.use('/', pagesRouter)
-app.use('/api/register', registerRouter)
-app.use(`/api/exercises`, exercisePageRouter)
-
-
-
-// app.use('/api/exercises', exercises);
-// app.use('/api/login', login);
-// app.use('/api/statistics', statistics);
-// app.use('/', pages);
-// app.use('/api/register', register);
+app.use('/api/exercises', exercisesRouter);
+app.use('/api/statistics', statisticsRouter);
+app.use('/api/login', loginRouter);
+app.use('/', pagesRouter);
+app.use('/api/register', registerRouter);
+app.use(`/api/exercises`, exercisePageRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
--- public/register/script.js
+++ public/register/script.js
@@ -1,43 +1,47 @@
+document.getElementById('registrationForm').addEventListener('submit', function (event) {
+  event.preventDefault();
 
-document.getElementById('registrationForm').addEventListener('submit', function(event) {
-    console.log('dsad')
-    event.preventDefault();
-  
-    var firstName = document.getElementById('firstName').value;
-    var surname = document.getElementById('surname').value;
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
-  
-    var data = {
-        firstName: firstName,
-        surname: surname,
-        email: email,
-        password: password
-    };
-    console.log(data)
+  console.log('Before fetch');  // Перемещаем сюда
+  var firstName = document.getElementById('firstName').value;
+  var email = document.getElementById('email').value;
+  var password = document.getElementById('password').value;
 
-    fetch('/api/register', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-       })
-       .then(response => {
+  var errorMessage = '';
+
+  // If no errors, proceed with registration
+  var data = {
+     firstName: firstName,
+     email: email,
+     password: password
+  };
+
+  console.log('Before fetch');
+
+  fetch('/api/register', {
+     method: 'POST',
+     headers: {
+        'Content-Type': 'application/json'
+     },
+     body: JSON.stringify(data)
+  })
+     .then(response => {
+      console.log("aaaaa")
+      console.log('Response received');
         if (!response.ok) {
-          throw new Error('You already have an account on leetcode');
+           console.log("Already have account")
+           throw new Error('You already have an account!');
         }
         return response.json();
-      })
-      .then(data => {
+     })
+     .then(data => {
         console.log(data);
         document.getElementById('error-message').textContent = '';
-  
         window.location.href = '/exercises';
-      })
-      .catch(error => {
+     })
+     .catch(error => {
+        console.log('aaaa')
+        console.log(error.message)
         console.error(error.message);
-        
         document.getElementById('error-message').textContent = error.message;
-      });
- });
+     });
+});
