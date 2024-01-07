9cb8362a06c710bfbf09136d2a435f1f9662795e
Marcin Lisowski <mnlisows@gmail.com>
2024-01-07 02:58:32 +0100

    added route for toggle state

--- routers/profile.js
+++ routers/profile.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const {get_aggregate_stats, get_calendar, get_history} = require('../controllers/profileController')
+const {get_aggregate_stats, get_calendar, get_history, toggle_state} = require('../controllers/profileController')
 
 router.route('/aggregate').get(get_aggregate_stats);
 router.route('/calendar').get(get_calendar);
 router.route('/history').get(get_history);
+router.route('/toggleState').post(toggle_state);
 
 
 module.exports = router
