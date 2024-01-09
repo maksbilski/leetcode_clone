f73c0561b4f92f4889b5cd60981c946a844915fc
Marcin Lisowski <mnlisows@gmail.com>
2024-01-09 20:23:16 +0100

    assigining current date to last_login in db

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -8,13 +8,19 @@ const login_user = async (req, res) => {
   
       console.log(`Email: ${email}`);
       console.log(`Password: ${password}`);
-  
+      const currentDate = new Date().toISOString().split('T')[0];
       const result = await pool`SELECT * FROM users WHERE email = ${email} AND password = ${password}`;
   
       console.log(result)
       console.log('Result.rows:', result.length);
   
       if (result.length > 0) {
+        console.log('changing login')
+        const change_date = await pool`
+        UPDATE users
+        set last_login = ${currentDate}
+        where email = ${email};
+        `;
         const userId = result[0].user_id; 
         console.log('User ID:', userId);
         req.session.userId = userId;
