32af703fb8fa5aec8529f898acd5e8e0e669b718
mrozek <01178582@pw.edu.pl>
2024-01-10 22:29:36 +0100

    status implemented

--- controllers/exercisesController.js
+++ controllers/exercisesController.js
@@ -17,6 +17,7 @@ const getExercises = async (req, res) => {
 };
 
 const sortExercises = async (req, res) => {
+  const userId = req.session.userId;
   try {
     const validKeys = ["exercise_id", "name", "category", "difficulty"];
     const { key } = req.query;
@@ -24,9 +25,11 @@ const sortExercises = async (req, res) => {
       return res.status(400).json({ error: 'Invalid sort key' });
     }
     const result = await pool`
-      SELECT * 
-      FROM exercises 
-      ORDER BY ${pool(key)};`;
+    SELECT e.exercise_id, e.name, e.category, e.difficulty, e.date_added, exu.done, exu.success 
+    FROM exercises e 
+    LEFT JOIN ex_users exu ON (e.exercise_id = exu.exercise_id AND exu.user_id = ${userId})
+    ORDER BY ${pool(key)}
+    `;
     res.json(result);
   } catch (error) {
     console.error(error);
