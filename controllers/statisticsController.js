39a849de0b6d2ff9917df420479b72b78986b8a0
Maksymilian Bilski <maksde25@gmail.com>
2024-01-10 22:36:07 +0100

    Modify statistics functions for new column name

--- controllers/statisticsController.js
+++ controllers/statisticsController.js
@@ -10,30 +10,30 @@ const sort_statistics = async (req, res) => {
     }
     const result = await pool`
       WITH UserExercisePercentiles AS (
-        SELECT 
+        SELECT
           ex_users.user_id,
           ex_users.exercise_id,
-          ROUND(PERCENT_RANK() OVER (PARTITION BY ex_users.exercise_id ORDER BY ex_users.speed) * 100) AS exercise_percentile
-        FROM 
-          ex_users 
-        WHERE 
+          ROUND(PERCENT_RANK() OVER (PARTITION BY ex_users.exercise_id ORDER BY ex_users.run_time) * 100) AS exercise_percentile
+        FROM
+          ex_users
+        WHERE
           ex_users.success = true
       )
-      SELECT 
+      SELECT
         users.name,
         users.user_id,
         COUNT(ex_users.exercise_id) AS total_exercises_attempted,
         COUNT(CASE WHEN ex_users.success = true THEN 1 END) AS total_exercises_completed,
         ROUND(COUNT(CASE WHEN ex_users.done = true AND ex_users.success = true THEN 1 END) * 100.0 / COUNT(ex_users.exercise_id), 1) AS success_rate,
         CONCAT(ROUND(100 - AVG(UserExercisePercentiles.exercise_percentile)), '%') AS average_percentile
-      
-      FROM 
+
+      FROM
         users
-      JOIN 
+      JOIN
         ex_users ON users.user_id = ex_users.user_id
-      LEFT JOIN 
+      LEFT JOIN
         UserExercisePercentiles ON users.user_id = UserExercisePercentiles.user_id
-      GROUP BY 
+      GROUP BY
         users.user_id
       ORDER BY ${pool(key)} DESC;`;
     res.json(result);
@@ -47,32 +47,32 @@ const get_statistics = async (req, res) => {
   try {
     const result = await pool`
       WITH UserExercisePercentiles AS (
-        SELECT 
+        SELECT
           ex_users.user_id,
           ex_users.exercise_id,
-          ROUND(PERCENT_RANK() OVER (PARTITION BY ex_users.exercise_id ORDER BY ex_users.speed) * 100) AS exercise_percentile
-        FROM 
-          ex_users 
-        WHERE 
+          ROUND(PERCENT_RANK() OVER (PARTITION BY ex_users.exercise_id ORDER BY ex_users.run_time) * 100) AS exercise_percentile
+        FROM
+          ex_users
+        WHERE
           ex_users.success = true
       )
-      SELECT 
-        users.user_id, 
-        users.name, 
+      SELECT
+        users.user_id,
+        users.name,
         COUNT(ex_users.exercise_id) AS total_exercises_attempted,
         COUNT(CASE WHEN ex_users.success = true THEN 1 END) AS total_exercises_completed,
         ROUND(COUNT(CASE WHEN ex_users.done = true AND ex_users.success = true THEN 1 END) * 100.0 / COUNT(ex_users.exercise_id), 1) AS success_rate,
         CONCAT(ROUND(100 - AVG(UserExercisePercentiles.exercise_percentile)), '%') AS average_percentile
-	
-      FROM 
+
+      FROM
         users
-      JOIN 
+      JOIN
         ex_users ON users.user_id = ex_users.user_id
-      LEFT JOIN 
+      LEFT JOIN
         UserExercisePercentiles ON users.user_id = UserExercisePercentiles.user_id
-      GROUP BY 
+      GROUP BY
         users.user_id
-      ORDER BY 
+      ORDER BY
         total_exercises_completed DESC;`;
     res.json(result);
   } catch (error) {
@@ -82,6 +82,6 @@ const get_statistics = async (req, res) => {
 };
 
 module.exports = {
-  get_statistics, 
+  get_statistics,
   sort_statistics
 };
