4f0e89b4a037713028c926ec17a260bb0473ae3f
Maksymilian Bilski <maksde25@gmail.com>
2024-01-10 22:35:52 +0100

    Implement submitting code

--- controllers/exercisePageController.js
+++ controllers/exercisePageController.js
@@ -10,13 +10,13 @@ const getExercisePage = async (req, res) => {
   try {
     const exerciseId = req.params.exercise_id;
     const result = await pool`
-      SELECT * 
+      SELECT *
       FROM exercises e
       INNER JOIN exercise_content ec ON (e.exercise_id = ec.exercise_id)
       WHERE e.exercise_id = ${exerciseId}`;
     res.json(result[0]);
   } catch (error) {
-    console.error(error);  
+    console.error(error);
     res.status(500).json({ error: 'Internal server error' });
   }
 };
@@ -37,7 +37,7 @@ const postLike = async (req, res) => {
       SET vote = ${vote};`;
 
     const result = await pool`
-      SELECT 
+      SELECT
         SUM(CASE WHEN vote = 1 THEN 1 ELSE 0 END) AS likes,
         SUM(CASE WHEN vote = -1 THEN 1 ELSE 0 END) AS dislikes
       FROM ex_users
@@ -56,7 +56,7 @@ const getLike = async (req, res) => {
   try {
     const exerciseId = req.params.exercise_id;
     const result = await pool`
-      SELECT 
+      SELECT
         SUM(CASE WHEN vote = 1 THEN 1 ELSE 0 END) AS likes,
         SUM(CASE WHEN vote = -1 THEN 1 ELSE 0 END) AS dislikes
       FROM ex_users
@@ -84,14 +84,63 @@ const getExerciseComments = async (req, res) => {
 	}
 }
 
+
 const runCode = async (req, res) => {
+  try {
+    const userId = req.session.userId;
+		const { exerciseId, code } = req.body;
+		const result = await executeCode(userId, exerciseId, code);
+		res.json({
+      message: 'Code executed',
+			output: result.stdout,
+			errorOutput: result.stderr
+		});
+	} catch (error) {
+    console.error(error);
+		res.status(500).json({ error: 'Internal server error' });
+	}
+}
+
+const executeCode = async (userId, exerciseId, code) => {
+  const relativePath = `../tests/test_${exerciseId}.py`
+  const filePath = getAbsolutePath(relativePath)
+  const programPath = await createTemporaryFile(code, exerciseId, userId);
+  const result = await runDockerContainer(filePath, programPath);
+  return result;
+}
+
+const submitCode = async (req, res) => {
 	try {
 		const userId = req.session.userId;
 		const { exerciseId, code } = req.body;
-		const relativePath = `../tests/${exerciseId}.py`
-		const filePath = getAbsolutePath(relativePath)
-		const programPath = await createTemporaryFile(code, exerciseId, userId);
-		const result = await runDockerContainer(filePath, programPath);
+		const result = await executeCode(userId, exerciseId, code);
+    const output = result.stdout;
+    const regex = /Total runtime: (\d+\.\d+)/;
+    const match = output.match(regex);
+    const runTime = parseFloat(match[1]);
+    console.log(runTime);
+    console.log(exerciseId);
+    console.log(userId);
+    console.log(!result.error);
+    const insert = await pool
+    `INSERT INTO ex_users (user_id, exercise_id, run_time, done, success)
+    VALUES (${userId}, ${exerciseId}, ${runTime}, ${true}, ${!result.error})
+    ON CONFLICT (user_id, exercise_id) DO UPDATE
+    SET run_time = ${runTime}, done = ${true}, success = ${!result.error};`;
+    const countOfWorseSolutions = await pool
+    `
+    SELECT COUNT(exercise_id) AS count
+    FROM ex_users
+    WHERE run_time > ${runTime} AND exercise_id = ${exerciseId} AND success = ${true};
+    `;
+    const countOfSolutions = await pool
+    `
+    SELECT COUNT(exercise_id) AS count
+    FROM ex_users
+    WHERE exercise_id = ${exerciseId} AND success = ${true};
+    `;
+    console.log(countOfWorseSolutions[0].count);
+    console.log(countOfSolutions[0].count);
 		res.json({
 			message: 'Code executed',
 			output: result.stdout,
@@ -102,7 +151,6 @@ const runCode = async (req, res) => {
 		res.status(500).json({ error: 'Internal server error' });
 	}
 }
-
 
 function getAbsolutePath(relativePath) {
 	return path.resolve(__dirname, relativePath);
@@ -142,7 +190,8 @@ function runDockerContainer(testPath, programPath) {
 module.exports = {
 	getExercisePage,
 	getExerciseComments,
+  submitCode,
 	runCode,
-  	postLike,
-  	getLike,
+  postLike,
+  getLike,
 };
