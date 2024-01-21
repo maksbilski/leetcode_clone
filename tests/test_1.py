304613e4671aaf2b30311ebb50463010087d0b50
Maksymilian Bilski <maksde25@gmail.com>
2024-01-21 15:27:58 +0100

    Add tests for first exercise

--- tests/test_1.py
+++ tests/test_1.py
@@ -4,56 +4,56 @@
 from solution import Solution
 import pytest
 
-def test_bubble_sort_unsorted_list():
-    solution = Solution()
-    assert solution.bubble_sort([3, 2, 1]) == [1, 2, 3]
 
-def test_bubble_sort_sorted_list():
+def test_longest_substring_unique_chars():
     solution = Solution()
-    assert solution.bubble_sort([1, 2, 3]) == [1, 2, 3]
+    assert solution.lengthOfLongestSubstring("abcabcbb") == 3
 
-def test_bubble_sort_reverse_list():
+def test_longest_substring_all_same_chars():
     solution = Solution()
-    assert solution.bubble_sort([3, 2, 1]) == [1, 2, 3]
+    assert solution.lengthOfLongestSubstring("bbbbb") == 1
 
-def test_bubble_sort_identical_elements():
+def test_longest_substring_mixed_chars():
     solution = Solution()
-    assert solution.bubble_sort([1, 1, 1]) == [1, 1, 1]
+    assert solution.lengthOfLongestSubstring("pwwkew") == 3
 
-def test_bubble_sort_single_element():
+def test_longest_substring_empty_string():
     solution = Solution()
-    assert solution.bubble_sort([1]) == [1]
+    assert solution.lengthOfLongestSubstring("") == 0
 
-def test_bubble_sort_empty_list():
+def test_longest_substring_no_repeats():
     solution = Solution()
-    assert solution.bubble_sort([]) == []
+    assert solution.lengthOfLongestSubstring("abcdef") == 6
 
-def test_bubble_sort_floats():
+def test_longest_substring_repeating_chars():
     solution = Solution()
-    assert solution.bubble_sort([1.5, 0.5, 2.5]) == [0.5, 1.5, 2.5]
+    assert solution.lengthOfLongestSubstring("aab") == 2
 
-def test_bubble_sort_large_list():
+def test_longest_substring_long_string():
     solution = Solution()
-    large_list = list(range(100, 0, -1))
-    sorted_list = list(range(1, 101))
-    assert solution.bubble_sort(large_list) == sorted_list
+    long_string = "a" * 10000 + "b" * 10000
+    assert solution.lengthOfLongestSubstring(long_string) == 2
 
-def test_bubble_sort_with_duplicates():
+def test_longest_substring_complex_case():
     solution = Solution()
-    assert solution.bubble_sort([3, 2, 1, 3, 2]) == [1, 2, 2, 3, 3]
+    assert solution.lengthOfLongestSubstring("dvdf") == 3
 
-def test_bubble_sort_extreme_values():
+def test_longest_substring_single_char_string():
     solution = Solution()
-    assert solution.bubble_sort([-10000, 10000, 0]) == [-10000, 0, 10000]
+    assert solution.lengthOfLongestSubstring("a") == 1
 
-def test_bubble_sort_integers_and_floats():
+def test_longest_substring_with_spaces():
     solution = Solution()
-    assert solution.bubble_sort([3.5, 2, 1.1, 3]) == [1.1, 2, 3, 3.5]
+    assert solution.lengthOfLongestSubstring(" ") == 1
 
-def test_bubble_sort_all_positive():
+def test_longest_substring_with_symbols():
     solution = Solution()
-    assert solution.bubble_sort([10, 20, 30, 40]) == [10, 20, 30, 40]
+    assert solution.lengthOfLongestSubstring("!@#$%^&*()") == 10
 
-def test_bubble_sort_all_negative():
+def test_longest_substring_with_numbers():
     solution = Solution()
-    assert solution.bubble_sort([-10, -20, -30, -40]) == [-40, -30, -20, -10]
+    assert solution.lengthOfLongestSubstring("11234567890") == 10
+
+def test_longest_substring_with_mixed_characters():
+    solution = Solution()
+    assert solution.lengthOfLongestSubstring("a1b2c3d4e5f6g7h8i9j0") == 20
