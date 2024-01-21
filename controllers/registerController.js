8e5eca3ddf65826b970e5fb80f8c3f9664b6a896
Maksymilian Bilski <maksde25@gmail.com>
2024-01-21 18:37:24 +0100

    update regsiterUser controller function so it hashes the password before storing it in the database

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -1,29 +1,30 @@
 const pool = require('../db');
+const bcrypt = require('bcrypt');
 
 const registerUser = async (req, res) => {
-  console.log('Trying to register');
   try {
-    console.log(req.body);
     const { firstName, email, password } = req.body;
     const name = `${firstName}`;
-    const is_exist = await pool`SELECT * FROM users WHERE email = ${email} AND password = ${password}`;
-    console.log(is_exist);
-    
-    if (is_exist.length === 0) {
-      const current_date = new Date().toISOString();
-      const result = await pool`
-        INSERT INTO users (name, email, password, private, last_login)
-        VALUES (${name}, ${email}, ${password}, false, ${current_date});
-      `;
-      const check = await pool`SELECT * FROM users WHERE email = ${email} AND password = ${password}`;
-      console.log(check);
-      const userId = check[0].user_id;
-      req.session.userId = userId;
-      res.status(200).json('User registered successfully');
-    } else {
-      console.log('Already have an account');
-      res.status(401).json({ error: 'You already have an account on leetcode' });
+
+    const is_exist = await pool`SELECT * FROM users WHERE email = ${email}`;
+    if (is_exist.length > 0) {
+      return res.status(401).json({ error: 'You already have an account' });
     }
+
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    const current_date = new Date().toISOString();
+    await pool`
+      INSERT INTO users (name, email, password_hash, private, last_login)
+      VALUES (${name}, ${email}, ${hashedPassword}, false, ${current_date});
+    `;
+
+    const newUser = await pool`SELECT * FROM users WHERE email = ${email}`;
+    const userId = newUser[0].user_id;
+
+    req.session.userId = userId;
+    res.status(200).json('User registered successfully');
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Internal Server Error' });
